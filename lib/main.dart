19043590db62107037844eca9b2ce8951c5d493b
Mando CV <210124@utxicotepec.edu.mx>
2024-11-08 11:00:03 -0600

    Practica 23
    Seccion 6

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:yesnoapp_210124/config/theme/app_theme.dart';
 import 'package:yesnoapp_210124/presentation/screens/chat/chat_screen.dart';
+import 'package:yesnoapp_210124/presentation/providers/chat_provider.dart';
+
 
 void main() => runApp(const MyApp());
 
@@ -9,12 +12,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Yes No App',
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme( selectedColor: 1).theme(),
-      home: const ChatScreen()
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => ChatProvider() )
+      ],
+      child: MaterialApp(
+        title: 'Yes No App',
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme( selectedColor: 0 ).theme(),
+        home: const ChatScreen()
+      ),
     );
   }
 }
+
+
 
--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\ti_la\Desktop\YesNoApp-210124\yesnoapp_210124"
+export "FLUTTER_APPLICATION_PATH=C:\Users\PC 19\Desktop\YesNoApp-210124"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
